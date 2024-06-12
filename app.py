f7ad9eb3119f2ff98cae492d429ef5853603d3bc
zeddkhan03 <zeddkhan03@gmail.com>
2024-06-12 23:52:51 +0530

    added

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
+import openai
 import streamlit as st
 from PyPDF2 import PdfReader, PdfWriter 
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_community.chains.question_answering import load_qa_chain
-from langchain_community.llms import OpenAI
-from langchain_community.callbacks.manager import get_openai_callback
-
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 def ChatPDF(text):
     # st.write(text)
