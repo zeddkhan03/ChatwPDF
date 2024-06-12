b2bd3354d1d2749afea1a906b2e3e217a1e482be
zeddkhan03 <zeddkhan03@gmail.com>
2024-06-12 23:47:44 +0530

    added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import streamlit as st
 from PyPDF2 import PdfReader, PdfWriter 
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
